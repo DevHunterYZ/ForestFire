0f81f87c0e8934db8089aa6a4cf3253767909aa8
DevHunter <32465888+DevHunterYZ@users.noreply.github.com>
2021-12-22 01:21:10 +0300

    Create ForestFire.py

---
+++ ForestFire.py
@@ -0,0 +1,15 @@
+import keras 
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
+from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+import matplotlib.pyplot as plt
+from glob import glob
+import pandas as pd
+
+train_path = ".../Yangın"
+test_path = ".../Normal"
+
+img = load_img(".../Yangın/1.jpg")
+plt.imshow(img)
+plt.axis("off")
