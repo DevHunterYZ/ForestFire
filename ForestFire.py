8637b1a4d6610af2b472f01da11095e5eb0b19e8
DevHunter <32465888+DevHunterYZ@users.noreply.github.com>
2022-03-07 17:40:50 +0300

    Update ForestFire.py

--- ForestFire.py
+++ ForestFire.py
@@ -21,3 +21,50 @@ print(x.shape)
 className = glob(train_path + '/*')
 numberOfClass = len(className)
 print("NumberOfClass: ", numberOfClass)
+
+#%% CNN Model
+model = Sequential()
+model.add(Conv2D(32,(3,3),input_shape = x.shape))
+model.add(Activation("relu"))
+model.add(MaxPooling2D())
+
+model.add(Flatten())
+model.add(Dense(16))
+model.add(Activation("relu"))
+model.add(Dropout(0.5))
+model.add(Dense(numberOfClass))
+model.add(Activation("softmax"))
+
+model.compile(loss = "categorical_crossentropy",
+              optimizer = "rmsprop",
+              metrics = ["accuracy"])
+batch_size = 32
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.3,
+                                   horizontal_flip = True,
+                                   zoom_range = 0.3)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_path,
+    target_size=x.shape[:2],
+    batch_size = batch_size,
+    color_mode = "rgb",
+    class_mode = "categorical")             
+        
+
+test_generator = test_datagen.flow_from_directory(
+    test_path,
+    target_size = x.shape[:2],
+    batch_size = batch_size,
+    color_mode = "rgb",
+    class_mode = "categorical")
+                  
+model.fit_generator(
+    generator = train_generator,
+    steps_per_epoch = 32//batch_size,
+    epochs = 8,
+    validation_data = test_generator,
+    validation_steps = 8 // batch_size)
